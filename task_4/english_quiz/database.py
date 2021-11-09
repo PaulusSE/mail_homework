0e2d3c6966408ee396c802dec66b096ab7dc8ffb
Pavel Semenov <92322633+PaulusSE@users.noreply.github.com>
2021-11-09 19:50:04 +0300

    Add files via upload

---
+++ task_4/english_quiz/database.py
@@ -0,0 +1,10 @@
+words_database = {
+                     1: {'word_id': 1, 'en_word': 'python', 'ru_word': 'питон',
+                         'user': 'admin', 'word_img': 'img/python.jpg'},
+                     2: {'word_id': 2, 'en_word': 'Oleg', 'ru_word': 'Олег',
+                         'user': 'admin', 'word_img': 'img/lego.png'},
+                     3: {'word_id': 3, 'en_word': 'Cat', 'ru_word': 'Кот',
+                         'user': 'admin', 'word_img': 'img/nyan_cat.gif'},
+                     4: {'word_id': 4, 'en_word': 'Home', 'ru_word': 'Дом',
+                         'user': 'admin', 'word_img': ''},
+}
